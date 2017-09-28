efd391b9ee2c54710b4939eeac0aa61065208860
Matthew Carney <matthewcarney64@gmail.com>
2017-09-28 17:35:01 +0100

    Updated commands to use sudo

--- install_mariadb_instance.sh
+++ install_mariadb_instance.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Install required packages
-apt-get update -y
-apt-get install -y sudo mariadb-server
+sudo apt-get update -y
+sudo apt-get install -y sudo mariadb-server
 
 # Configure mariadb
-service mysql stop
-mysql_install_db
-'/usr/bin/mysqladmin' -u root password 'password'
-service mysql start
-(echo "password"; echo "n"; echo "n"; echo "n"; echo "n"; echo "n";) | mysql_secure_installation
+sudo service mysql stop
+sudo mysql_install_db
+sudo '/usr/bin/mysqladmin' -u root password 'password'
+sudo service mysql start
+(echo "password"; echo "n"; echo "n"; echo "n"; echo "n"; echo "n";) | sudo mysql_secure_installation
 
 # Create and login as new user
-adduser --system --home /home/donate --disabled-password --shell /bin/bash donate
-(echo "password"; echo "password"; ) | passwd donate
+sudo adduser --system --home /home/donate --disabled-password --shell /bin/bash donate
+(echo "password"; echo "password"; ) | sudo passwd donate
 sudo usermod -aG sudo donate
 sudo su - donate
