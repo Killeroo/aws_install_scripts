6a997c74851681ca2d1b727df5cdbce33b43c28e
Matthew Carney <matthewcarney64@gmail.com>
2017-10-09 15:10:42 +0100

    Added install_mariadb_galera_cluster_base.sh

---
+++ install_mariadb_galera_cluster_base.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Install prerequisuite packages for a active-passive galera cluster
+
+# Get basic packages 
+apt-get update -y
+apt-get install -y nano git software-properties-common rsync sudo
+
+# Install most recent version of mariadb-server
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu xenial main'
+apt-get update
+(echo password; echo "password";) | apt-get install -y mariadb-server
+
+# Create account and login
+adduser --system --home /home/galera-node --disabled-password --shell /bin/bash donate
+(echo "password"; echo "password"; ) | passwd galera-node
+usermod -aG sudo galera-node
+su - galera-node
