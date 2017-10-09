117cd34d86da70d3109a052664051ae92ad125c3
Matthew Carney <matthewcarney64@gmail.com>
2017-10-09 17:58:56 +0100

    added galera config and script for starting galera_node

--- install_mariadb_galera_cluster_base.sh
+++ install_mariadb_galera_cluster_base.sh
@@ -2,14 +2,17 @@
 # Install prerequisuite packages for a active-passive galera cluster
 
 # Arguments check
-if [ $# -eq 0 ]; then
-	echo "Please specify node number";
+if [ $# -lt 2 ]; then
+	echo "Not enough arguments: script [node_num] [node_ip]";
 	exit;
+else
+	user="galera-node-$1"
+	node_ip=$2
 fi
 
 # Get basic packages 
 apt-get update -y
-apt-get install -y nano git software-properties-common rsync sudo
+apt-get install -y nano git software-properties-common rsync sudo ufw
 
 # Install most recent version of mariadb-server
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
@@ -17,9 +20,24 @@ add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitaloce
 apt-get update
 apt-get install -y mariadb-server
 
+# Copy and update galera config
+mv ./galera.cnf /etc/mysql/conf.d/galera.cnf
+echo "wsrep_node_address=\"$node_ip\"" >> /etc/mysql/conf.d/galera.cnf
+echo "wsrep_node_name=\"$user\"" >> /etc/mysql/conf.d/galera.cnf
+echo "wsrep_cluster_address=\"gcomm://first_ip,second_ip,third_ip\"" >> /etc/mysql/conf.d/galera.cnf
+
+# Setup galera firewalls exceptions (and SSH and )
+ufw enable
+ufw allow 3306,4444,4567,4568/tcp
+ufw allow 4567/udp
+ufw allow 22/tcp
+ufw status
+
 # Create account and login
-user="galera-node-$1"
 adduser --system --home /home/$user --disabled-password --shell /bin/bash $user
 (echo "password"; echo "password"; ) | passwd $user
 usermod -aG sudo $user
 su - $user
+
+echo "**REMEMBER TO REPLACE wsrep_cluster_address=\"gcomm://first_ip,second_ip,third_ip\"**";
+echo "In /etc/mysql/conf.d/galera.cnf"
---
+++ start_galera_node.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Stop mysql service
+sudo systemctl stop mysql
+
+# Setup new cluster
+sudo galera_new_cluster
+
+# Restart mysql
+sudo systemctl start mysql
+
+# Display cluster info
+mysql -u root -p -e "SHOW STATUS LIKE 'wsrep_cluster_size'"
