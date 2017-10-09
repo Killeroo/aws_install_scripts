d631bb775f34be895adfa7723fc9b0f12686340d
Matthew Carney <matthewcarney64@gmail.com>
2017-10-09 15:15:13 +0100

    fixed user creation bug

--- install_mariadb_galera_cluster_base.sh
+++ install_mariadb_galera_cluster_base.sh
@@ -9,10 +9,10 @@ apt-get install -y nano git software-properties-common rsync sudo
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu xenial main'
 apt-get update
-(echo password; echo "password";) | apt-get install -y mariadb-server
+apt-get install -y mariadb-server
 
 # Create account and login
-adduser --system --home /home/galera-node --disabled-password --shell /bin/bash donate
+adduser --system --home /home/galera-node --disabled-password --shell /bin/bash galera-node
 (echo "password"; echo "password"; ) | passwd galera-node
 usermod -aG sudo galera-node
 su - galera-node
