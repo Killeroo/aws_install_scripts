0ca135cd907c475159d6db36c6a26fdae6c71261
Matthew Carney <matthewcarney64@gmail.com>
2017-10-10 10:09:44 +0100

    added sudo to install_mariadb_galera_cluster_base.sh renamed install_mariadb_instance.sh to install_mariadb_generic_instance.sh

--- install_mariadb_galera_cluster_base.sh
+++ install_mariadb_galera_cluster_base.sh
@@ -11,27 +11,27 @@ else
 fi
 
 # Get basic packages 
-apt-get update -y
-apt-get install -y nano git software-properties-common rsync sudo ufw
+sudo apt-get update -y
+sudo apt-get install -y nano git software-properties-common rsync sudo ufw
 
 # Install most recent version of mariadb-server
-apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.utexas.edu/mariadb/repo/10.1/ubuntu xenial main';#'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu xenial main'
-apt-get update
-apt-get install -y mariadb-server
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.utexas.edu/mariadb/repo/10.1/ubuntu xenial main';#'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu xenial main'
+sudo apt-get update
+sudo apt-get install -y mariadb-server
 
 # Copy and update galera config
-mv ./galera.cnf /etc/mysql/conf.d/galera.cnf
+sudo mv ./galera.cnf /etc/mysql/conf.d/galera.cnf
 echo "wsrep_node_address=\"$node_ip\"" >> /etc/mysql/conf.d/galera.cnf
 echo "wsrep_node_name=\"$user\"" >> /etc/mysql/conf.d/galera.cnf
 echo "wsrep_cluster_address=\"gcomm://first_ip,second_ip,third_ip\"" >> /etc/mysql/conf.d/galera.cnf
 
 # Setup galera firewalls exceptions (and SSH and )
-ufw enable
-ufw allow 3306,4444,4567,4568/tcp
-ufw allow 4567/udp
-ufw allow 22/tcp
-ufw status
+sudo ufw enable
+sudo ufw allow 3306,4444,4567,4568/tcp
+sudo ufw allow 4567/udp
+sudo ufw allow 22/tcp
+sudo ufw status
 
 echo
 echo "#####################################################################################";
@@ -40,7 +40,7 @@ echo "  In /etc/mysql/conf.d/galera.cnf"
 echo "#####################################################################################";
 
 # Create account and login
-adduser --system --home /home/$user --disabled-password --shell /bin/bash $user
-(echo "password"; echo "password"; ) | passwd $user
-usermod -aG sudo $user
-su - $user
+sudo adduser --system --home /home/$user --disabled-password --shell /bin/bash $user
+(echo "password"; echo "password"; ) | sudo passwd $user
+sudo usermod -aG sudo $user
+sudo su - $user
