de01b0b506f41be9900e3d2cb779262a848aa0f7
Matthew Carney <matthewcarney64@gmail.com>
2017-10-12 16:21:55 +0100

    fixed status

--- show_galera_status.sh
+++ show_galera_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Show mysql/maria/galera service status
-sudo systemctl status mysql
+sudo systemctl --no-pager --lines=0 status mysql
 
 # Show cluster status
 sudo mysql -u root -p -e "SHOW STATUS LIKE 'wsrep_cluster_size'"
