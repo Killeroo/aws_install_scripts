c4937a93ca055490505e4ca832788782ae6a7a6d
Matthew Carney <matthewcarney64@gmail.com>
2017-10-17 12:14:42 +0100

    updated fetch_password.sh

--- fetch_password.sh
+++ fetch_password.sh
@@ -2,5 +2,18 @@
 
 # Takes mysql or account argument
 
+# Check if bash (shell) or sh (subshell) is running script
+[[ $_ != $0 ]] && echo "Script is being sourced" || echo "Script is a subshell"
+
+
+# Arguments check
+if [ $# -ne 1 ]; then
+	echo "Incorrect argument count:"
+	echo "fetch_password [mysql] OR [general]"
+	exit
+fi
+# Check password.conf exists
+# Check password.conf passwords is not empty
+
 # Fetch password from conf file
-grep 'password=' passwords.conf | awk -F= '{ print  $2 }'
+grep 'password=' $PWD/password.conf | awk -F= '{ print  $2 }'
