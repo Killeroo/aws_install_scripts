611420a2ec1131a0214d82bcd76509437c41f5c3
Matthew Carney <matthewcarney64@gmail.com>
2017-10-17 12:56:03 +0100

    Final touches to fetch_password.sh script

--- fetch_password.sh
+++ fetch_password.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Takes mysql or account argument
-
-# Check if script is being run with bash
-[[ $_ != $0 ]] && sourced=1 || sourced=0
-if [ $sourced -ne 1 ]; then
-	echo "Please use 'bash fetch_password.sh to run the script"
-	exit;
-fi
-
 # Arguments check
 if [ $# -ne 1 ]; then
 	echo "Incorrect argument count:"
@@ -23,8 +14,7 @@ if [ ! -f $PWD/password.conf ]; then
 	exit;
 fi
 
-# Check password.conf passwords is not empty
-
+# Fetch appropriate password
 if [ "$1" == "mysql" ]; then
 	grep 'mysql_password=' $PWD/password.conf | awk -F= '{ print  $2 }'
 elif [ "$1" == "account" ]; then
@@ -32,6 +22,3 @@ elif [ "$1" == "account" ]; then
 else
 	echo "Unrecognised argument: Please use either [mysql] OR [account]"
 fi
-
-# Fetch password from conf file
-grep 'password=' $PWD/password.conf | awk -F= '{ print  $2 }'
