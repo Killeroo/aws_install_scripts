86e03d66bbf50f82818247624a42255b65d88658
Matthew Carney <matthewcarney64@gmail.com>
2017-10-20 13:46:56 +0100

    Added enable_node_bootstrapping.sh for starting cluster again

---
+++ enable_node_bootstrapping.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+read -r -p "Continue? [Y/n] " response
+case "$response" in 
+	[yY][eE][sS]|[yY])
+		echo "yeah go on then"
+		;;
+	*)
+		echo "no thank you"
+		;;
+esac
+
+exit
+
+# Check if script is being run with bash
+[[ $_ != $0 ]] && sourced=1 || sourced=0
+if [ $sourced -ne 1 ]; then
+	echo "Please use 'bash enable_node_bootstrapping.sh to run the script"
+	exit;
+fi
+
+# Root check
+if [[ $EUID -ne 0 ]]; then
+	echo "Please run script as root"
+	exit;
+fi
+
+echo
+echo "###########################################################################";
+echo "                         ***WARNING***"
+echo "          ONLY RUN THIS SCRIPT ON THE MAIN CLUSTER NODE"
+echo " (It enables starting node even if it was not the last node to exit cluster)"
+echo "###########################################################################";
+echo 
+
+
+# Check file exists
+if [ ! -f /etc/mysql/conf.d/galera.cnf ]; then
+	echo "      ******Galera config file not found********"
+	echo "Run install_mariadb_galera_cluster_base.sh and try again"
+	echo
+	exit;
+fi
+
+sed -i "s|wsrep_cluster_address=.*|wsrep_cluster_address=\"gcomm://$1\"|" /etc/mysql/conf.d/galera.cnf
+echo "Updated safe_to_bootstrap -> 1"
