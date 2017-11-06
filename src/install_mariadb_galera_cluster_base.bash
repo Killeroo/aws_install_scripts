a02d7086b45888c772b2554df1032f2a98ac531b
Matthew Carney <matthewcarney64@gmail.com>
2017-11-06 16:53:58 +0000

    Added expect to mariadb installation

--- src/install_mariadb_galera_cluster_base.bash
+++ src/install_mariadb_galera_cluster_base.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Get config data
+cd .. & source $PWD/setup.cnf
+cd src
+
 # Get basic packages 
 sudo apt-get update -y
 sudo apt-get install -y nano git software-properties-common rsync sudo ufw expect
@@ -8,7 +12,13 @@ sudo apt-get install -y nano git software-properties-common rsync sudo ufw expec
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.utexas.edu/mariadb/repo/10.1/ubuntu xenial main';#'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu xenial main'
 sudo apt-get update
-sudo apt-get install -y mariadb-server
+# sudo apt-get install -y mariadb-server
+expect -c "
+spawn sudo apt-get install -y mariadb-server
+set timeout = 60
+expect -nocase \"*New password for the MariaDB \"root\" user:*\" {send \"$mysql_password\r\"; interact}
+expect -nocase \"*Repeat password for the MariaDB \"root\" user:*\" {send \"$mysql_password; interact}
+"
 
 # Copy galera config
 sudo cp $PWD/galera.cnf /etc/mysql/conf.d/galera.cnf
