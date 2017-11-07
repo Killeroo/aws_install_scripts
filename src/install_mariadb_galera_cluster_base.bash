9a705bde8400b8f8535554a02293c4b874db26c2
Matthew Carney <matthewcarney64@gmail.com>
2017-11-07 11:46:45 +0000

    Added mysql_secure_installation

--- src/install_mariadb_galera_cluster_base.bash
+++ src/install_mariadb_galera_cluster_base.bash
@@ -13,6 +13,7 @@ sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F2
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.utexas.edu/mariadb/repo/10.1/ubuntu xenial main';#'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu xenial main'
 sudo apt-get update
 sudo apt-get install -y mariadb-server
+sudo mysql_secure_installation
 
 # Copy galera config
 sudo cp $PWD/galera.cnf /etc/mysql/conf.d/galera.cnf
