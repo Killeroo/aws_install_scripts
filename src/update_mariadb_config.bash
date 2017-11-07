5698bba80d2d32d85d3e60c6b9e0ed3d81a35537
Matthew Carney <matthewcarney64@gmail.com>
2017-11-07 10:51:57 +0000

    Removed speech marks from update_mariadb_config script

--- src/update_mariadb_config.bash
+++ src/update_mariadb_config.bash
@@ -6,4 +6,4 @@ if [ $# -ne 2 ]; then
     exit;
 fi
 
-sed -i "s|$1.*|$1=\"$2\"|" /etc/mysql/my.cnf && echo "> \"$1 $2\" added to /etc/mysql/my.cnf "
+sed -i "s|$1.*|$1=$2|" /etc/mysql/my.cnf && echo "> \"$1 $2\" added to /etc/mysql/my.cnf "
